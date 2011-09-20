dcc4e67479c27303d5c486744da87e9aa78c2aa2
Matt Mastracci <matthew@mastracci.com>
2011-09-20 11:43:55 -0600

    Improve comments.

--- stencil/src/net/stencilproject/template/TemplateFileSourceInfo.java
+++ stencil/src/net/stencilproject/template/TemplateFileSourceInfo.java
@@ -1,11 +1,18 @@
 package net.stencilproject.template;
 
+/**
+ * {@link SourceInfo} implementation that represents the top-level of a
+ * {@link TemplateFile}.
+ */
 class TemplateFileSourceInfo implements SourceInfo {
 	private final TemplateFile file;
 	private final SourceInfo parent;
 	private final int line;
 	private final int column;
 
+	/**
+	 * Constructs the top-level {@link SourceInfo} for a {@link TemplateFile}.
+	 */
 	public TemplateFileSourceInfo(TemplateFile file) {
 		this.file = file;
 		this.parent = null;
@@ -13,6 +20,10 @@ class TemplateFileSourceInfo implements SourceInfo {
 		this.column = SourceInfo.NO_COLUMN;
 	}
 
+	/**
+	 * Constructs a nested {@link SourceInfo} where a {@link TemplateFile} has
+	 * been included from another {@link TemplateFile}.
+	 */
 	public TemplateFileSourceInfo(TemplateFile file, SourceInfo parent, int line, int column) {
 		this.file = file;
 		this.parent = parent;
